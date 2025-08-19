d4276810563e70ea2bc6caa33bd8ddf0386a4f7a
karlos23234 <israyelvardanyan48@gmail.com>
2025-08-19 14:48:21 +0400

    Update dash_watch_bot.py

--- dash_watch_bot.py
+++ dash_watch_bot.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+  port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 
